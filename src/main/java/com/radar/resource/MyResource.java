5b784ad4f8071865103a46c2e4bdb4d33d921656
GZhaoQing <ufoguozhaoqing@qq.com>
2018-01-22 18:18:27 +0800

    在MyResource增加一个数据接口

--- src/main/java/com/radar/resource/MyResource.java
+++ src/main/java/com/radar/resource/MyResource.java
@@ -1,6 +1,7 @@
 package com.radar.resource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.radar.Data4Json;
 import com.radar.FileParser;
 import com.radar.RadarFile;
 
@@ -49,18 +50,37 @@ public class MyResource {
     @Path("/files/{fileName}")
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.APPLICATION_JSON)
-    public String getData(@PathParam("fileName")String file , @Context ServletContext context) throws MalformedURLException {
+    public String getFileInfo(@PathParam("fileName")String file , @Context ServletContext context) throws MalformedURLException {
         FileParser p = new FileParser();
         try {
-            double ts = System.currentTimeMillis();
+//            double ts = System.currentTimeMillis();
             //KFWD_SDUS64_NCZGRK_201208150212,KEWX_SDUS54_N0VEWX_201707270600,SATE_L2_F2G_VISSR_MWB_LBT_SEC_LCN-IR2-20170527-0100.AWX
             String filePath = Thread.currentThread().getContextClassLoader().getResource(file).toString();
             String imagePath = context.getResource("img").getPath();
 
             RadarFile rf = p.readWithImg(filePath, imagePath);
 
-            double te = System.currentTimeMillis();
-            System.out.println(te - ts + "ms");
+//            double te = System.currentTimeMillis();
+//            System.out.println(te - ts + "ms");
+            ObjectMapper mapper = new ObjectMapper();
+            String j = mapper.writeValueAsString(rf);
+            return j;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    @GET
+    @Path("/filesD/{fileName}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String getData(@PathParam("fileName")String file , @Context ServletContext context) throws MalformedURLException {
+        FileParser p = new FileParser();
+        try {
+            String filePath = Thread.currentThread().getContextClassLoader().getResource(file).toString();
+
+            Data4Json rf = p.readGridData(filePath);
+
             ObjectMapper mapper = new ObjectMapper();
             String j = mapper.writeValueAsString(rf);
             return j;
