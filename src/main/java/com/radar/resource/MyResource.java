5a238845045f17f9db6674add109561dbe4cd031
GZhaoQing <ufoguozhaoqing@qq.com>
2018-01-31 21:01:13 +0800

    添加获取文件列表的资源接口
    现在可以获取数据文件的文件名列表了；同时把文件位置放在了新文件夹dataFiles

--- src/main/java/com/radar/resource/MyResource.java
+++ src/main/java/com/radar/resource/MyResource.java
@@ -1,5 +1,6 @@
 package com.radar.resource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.radar.Data4Json;
 import com.radar.FileParser;
@@ -10,6 +11,8 @@ import javax.servlet.ServletContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
@@ -17,32 +20,30 @@ import java.net.MalformedURLException;
 public class MyResource {
 
     @GET
-    @Path("/{fileName}")
+    @Path("/files/list")
     @Produces(MediaType.TEXT_PLAIN)
     @Consumes(MediaType.APPLICATION_JSON)
-    public String representTest(@PathParam("fileName")String file , @Context ServletContext context) throws MalformedURLException {
-        FileParser p=new FileParser();
-        try {
-            double ts=System.currentTimeMillis();
-            //KFWD_SDUS64_NCZGRK_201208150212,KEWX_SDUS54_N0VEWX_201707270600,SATE_L2_F2G_VISSR_MWB_LBT_SEC_LCN-IR2-20170527-0100.AWX
-            String filePath=Thread.currentThread().getContextClassLoader().getResource("KEWX_SDUS54_N0VEWX_201707270600" ).toString();
-            String imagePath=context.getResource("img").getPath();
+    public String getFileList(@Context ServletContext context) throws MalformedURLException, JsonProcessingException {
 
-            RadarFile rf=p.readWithImg(filePath,imagePath);
-
-            double te = System.currentTimeMillis();
-            System.out.println(te-ts+"ms");
-            ObjectMapper mapper=new ObjectMapper();
-            String j=mapper.writeValueAsString(rf);
-            return j;
-        } catch (IOException e) {
-            e.printStackTrace();
+        String filePath = context.getResource("WEB-INF\\classes\\dataFiles").getPath();
+        File f=new File(filePath);
+        File[] list = f.listFiles(new FileFilter() {
+            public boolean accept(File pathname) {
+//                if(){
+                    return true;
+//                }else{
+//                    return false;
+//                }
+            }
+        });
+        String[] array=new String[list.length];
+        int i=0;
+        for(File e:list){
+            array[i++]=e.getName();
         }
-        return null;
-
-//        return Thread.currentThread().getContextClassLoader().getResource("" ).toString()+"\n"+
-//                context.getResource("img").getPath();
-
+        ObjectMapper mapper = new ObjectMapper();
+        String j = mapper.writeValueAsString(array);
+        return j;
     }
 
 
@@ -55,7 +56,7 @@ public class MyResource {
         try {
 //            double ts = System.currentTimeMillis();
             //KFWD_SDUS64_NCZGRK_201208150212,KEWX_SDUS54_N0VEWX_201707270600,SATE_L2_F2G_VISSR_MWB_LBT_SEC_LCN-IR2-20170527-0100.AWX
-            String filePath = Thread.currentThread().getContextClassLoader().getResource(file).toString();
+            String filePath = Thread.currentThread().getContextClassLoader().getResource("dataFiles\\"+file).toString();
             String imagePath = context.getResource("img").getPath();
 
             RadarFile rf = p.readWithImg(filePath, imagePath);
@@ -77,7 +78,7 @@ public class MyResource {
     public String getData(@PathParam("fileName")String file , @Context ServletContext context) throws MalformedURLException {
         FileParser p = new FileParser();
         try {
-            String filePath = Thread.currentThread().getContextClassLoader().getResource(file).toString();
+            String filePath = Thread.currentThread().getContextClassLoader().getResource("dataFiles\\"+file).toString();
 
             Data4Json rf = p.readGridData(filePath);
 
